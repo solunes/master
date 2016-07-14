ae39a23d03b19ec03f6278d8e15bfdea17e477ef
Eduardo Mejia <edumejia30@gmail.com>
2016-07-14 11:06:11 -0400

    Cambios al Menu e Idiomas

--- src/app/Helpers/FuncNode.php
+++ src/app/Helpers/FuncNode.php
@@ -160,22 +160,22 @@ class FuncNode {
         $menu_array = \Solunes\Master\App\Menu::where('menu_type', 'admin')->where('level', 1)->lists('id');
         if($node->type=='site'&&!$node->parent_id){
             if(!$menu_site = \Solunes\Master\App\MenuTranslation::whereIn('menu_id', $menu_array)->where('name', 'Secciones')->first()){
-                $menu_site = \Solunes\Master\App\Menu::create(['type'=>'blank', 'menu_type'=>'admin', 'icon'=>'th-list', 'es'=>['name'=>'Secciones']]);
+                $menu_site = \Solunes\Master\App\Menu::create(['type'=>'blank', 'menu_type'=>'admin', 'permission'=>$node->permission, 'icon'=>'th-list', 'es'=>['name'=>'Secciones']]);
             }
             \Solunes\Master\App\Menu::create(['menu_type'=>'admin', 'parent_id'=>$menu_site->id, 'level'=>2, 'icon'=>'th-list', 'es'=>['name'=>$node->plural, 'link'=>'admin/model-list/'.$node->name]]);
         } else if($node->type=='system'&&!$node->parent_id) {
             if(!$menu_system = \Solunes\Master\App\MenuTranslation::whereIn('menu_id', $menu_array)->where('name', 'Sistema')->first()){
-                $menu_system = \Solunes\Master\App\Menu::create(['type'=>'blank', 'menu_type'=>'admin', 'permission'=>'system', 'icon'=>'th-list', 'es'=>['name'=>'Sistema']]);
+                $menu_system = \Solunes\Master\App\Menu::create(['type'=>'blank', 'menu_type'=>'admin', 'permission'=>$node->permission, 'icon'=>'th-list', 'es'=>['name'=>'Sistema']]);
             }
             \Solunes\Master\App\Menu::create(['menu_type'=>'admin', 'permission'=>$node->permission, 'parent_id'=>$menu_system->id, 'level'=>2, 'icon'=>'th-list', 'es'=>['name'=>$node->plural, 'link'=>'admin/model-list/'.$node->name]]);
         } else if($node->type=='global'&&!$node->parent_id) {
             if(!$menu_global = \Solunes\Master\App\MenuTranslation::whereIn('menu_id', $menu_array)->where('name', 'Global')->first()){
-                $menu_global = \Solunes\Master\App\Menu::create(['type'=>'blank', 'menu_type'=>'admin', 'permission'=>'global', 'icon'=>'th-list', 'es'=>['name'=>'Global']]);
+                $menu_global = \Solunes\Master\App\Menu::create(['type'=>'blank', 'menu_type'=>'admin', 'permission'=>$node->permission, 'icon'=>'th-list', 'es'=>['name'=>'Global']]);
             }
             \Solunes\Master\App\Menu::create(['menu_type'=>'admin', 'permission'=>$node->permission, 'parent_id'=>$menu_global->id, 'level'=>2, 'icon'=>'th-list', 'es'=>['name'=>$node->plural, 'link'=>'admin/model-list/'.$node->name]]);
         } else if($node->type=='form'&&!$node->parent_id) {
             if(!$menu_form = \Solunes\Master\App\MenuTranslation::whereIn('menu_id', $menu_array)->where('name', 'Forms')->first()){
-                $menu_form = \Solunes\Master\App\Menu::create(['type'=>'blank', 'menu_type'=>'admin', 'permission'=>'form', 'icon'=>'th-list', 'es'=>['name'=>'Forms']]);
+                $menu_form = \Solunes\Master\App\Menu::create(['type'=>'blank', 'menu_type'=>'admin', 'permission'=>$node->permission, 'icon'=>'th-list', 'es'=>['name'=>'Forms']]);
             }
             \Solunes\Master\App\Menu::create(['menu_type'=>'admin', 'permission'=>$node->permission, 'parent_id'=>$menu_form->id, 'level'=>2, 'icon'=>'th-list', 'es'=>['name'=>$node->plural, 'link'=>'admin/model-list/'.$node->name]]);
         }
