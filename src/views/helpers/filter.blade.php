b462b95facde94e64321d2535d8e8588cea14ca7
Eduardo Mejia <edumejia30@gmail.com>
2016-07-19 18:13:14 -0400

    Cambios en filtros

--- src/views/helpers/filter.blade.php
+++ src/views/helpers/filter.blade.php
@@ -5,10 +5,7 @@
         @if($field_parameter=='dates')
           {!! Field::form_input($i, $dt, ['name'=>'f_date_from','type'=>'string','required'=>false], ['cols'=>3]) !!}
           {!! Field::form_input($i, $dt, ['name'=>'f_date_to','type'=>'string','required'=>false], ['cols'=>3]) !!}
-        @elseif($field_parameter=='point')
-          {!! Field::form_input($i, $dt, ['name'=>'f_customer','type'=>'select','required'=>false, 'options'=>$search_customer], ['label'=>trans('fields.customer'), 'cols'=>3]) !!}
-          {!! Field::form_input($i, $dt, ['name'=>'f_point','type'=>'select','required'=>false, 'options'=>$search_point], ['label'=>trans('fields.point'), 'cols'=>3]) !!}
-        @elseif($field_parameter=='field'||$field_parameter=='parent_field'||$field_parameter=='custom')
+        @else
           {!! Field::form_input($i, $dt, ['name'=>'f_'.$field_name,'type'=>'select','required'=>false, 'options'=>$filter_options[$field_name]], ['label'=>trans('fields.'.$field_name), 'cols'=>3]) !!}
         @endif
       @endforeach
