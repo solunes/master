eabb3988633bb30bb55580ddb73d7cece3daa5b5
Eduardo Mejia <edumejia30@gmail.com>
2017-01-10 10:25:08 -0400

    Cambios a lightbox

--- src/views/scripts/lightbox-css.blade.php
+++ src/views/scripts/lightbox-css.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/lightbox-evolution/default/jquery.lightbox.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/admin/css/lightbox-evolution/default/jquery.lightbox.css') }}" />
 <!--[if IE 6]>
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/lightbox-evolution/default/jquery.lightbox.ie6.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/admin/css/lightbox-evolution/default/jquery.lightbox.ie6.css') }}" />
 <![endif]-->
