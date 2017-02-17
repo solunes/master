128ffe662508465aaad1505ea9587fe5986ee4cd
Eduardo Mejia <edumejia30@gmail.com>
2017-02-17 15:37:11 -0400

    Dynamic Forms Conditionals

--- src/app/Controllers/DynamicFormController.php
+++ src/app/Controllers/DynamicFormController.php
@@ -278,7 +278,7 @@ class DynamicFormController extends Controller {
             if(count(request()->input('conditionals_trigger_value'))>0){
                 $input_array = [];
                 foreach(request()->input('conditionals_trigger_value') as $input_key => $input){
-                    if($input&&$sub_field = \Solunes\Master\App\Field::where('name', request()->input('conditionals_trigger_field')[$input_key])->first())
+                    if($input&&$sub_field = \Solunes\Master\App\Field::where('name', request()->input('conditionals_trigger_field')[$input_key])->first()){
                       $subarray = [];
                       foreach(explode('|', $input) as $subinput){
                         if($sub_field_name = $sub_field->field_options()->whereTranslation('label', $subinput)->first()){
