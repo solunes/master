f664ff4a3a0aa04c2dcf161d2818561fef279e17
Eduardo Mejia <edumejia30@gmail.com>
2018-05-11 14:19:30 -0400

    Correccion filter checkbox to select

--- src/views/helpers/filter.blade.php
+++ src/views/helpers/filter.blade.php
@@ -30,6 +30,8 @@
           @elseif($field['subtype']=='string')
             {!! Field::form_input(NULL, $dt, ['name'=>'f_'.$field_name.'_action','type'=>'select','filter'=>$field['id'],'filter_delete'=>$field['show_delete'], 'options'=>$filter_string_options], ['label'=>$field['label'].' ('.trans('master::fields.action').')', 'cols'=>6, 'value'=>$filter_values['f_'.$field_name.'_action']]) !!}
             {!! Field::form_input(NULL, $dt, ['name'=>'f_'.$field_name,'type'=>'string','filter'=>$field['id'],'filter_delete'=>$field['show_delete']], ['label'=>$field['label'], 'cols'=>6, 'value'=>$filter_values['f_'.$field_name]]) !!}
+          @elseif(count($field['options'])>20)
+            {!! Field::form_input(NULL, $dt, ['name'=>'f_'.$field_name,'type'=>'select','filter'=>$field['id'],'filter_delete'=>$field['show_delete'], 'options'=>$field['options']], ['label'=>$field['label'], 'cols'=>12, 'value'=>$filter_values['f_'.$field_name]]) !!}
           @else
             {!! Field::form_input(NULL, $dt, ['name'=>'f_'.$field_name,'type'=>'checkbox','filter'=>$field['id'],'filter_delete'=>$field['show_delete'], 'options'=>$field['options']], ['label'=>$field['label'], 'cols'=>12, 'value'=>$filter_values['f_'.$field_name]]) !!}
           @endif
