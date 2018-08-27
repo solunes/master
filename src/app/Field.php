e91544be11c70bdf1e75b8e25bf84b96a6a8389c
Eduardo Mejia <edumejia30@gmail.com>
2018-08-27 18:54:49 -0400

    Correcciones

--- src/app/Field.php
+++ src/app/Field.php
@@ -108,16 +108,17 @@ class Field extends Model {
             if($subnode = \Solunes\Master\App\Node::where('name', $node_value)->first()){
                 $submodel = \FuncNode::node_check_model($subnode);
                 if(config('solunes.filter_subptions')){
-                  if(!isset(config('solunes.filter_subptions_exceptions')[$node_value])||config('solunes.filter_subptions_exceptions')[$node_value]==$field){
                     $fields = $subnode->fields()->where('type','select')->lists('name');
                     foreach($fields as $field){
                         $field_name = 'f_'.$field;
-                        if(request()->has($field_name)){
-                            $value = request()->input($field_name);
-                            $submodel = $submodel->where($field, $value);
+                        if(isset(config('solunes.filter_subptions_exceptions')[$node_value])&&config('solunes.filter_subptions_exceptions')[$node_value]==$field){
+                        } else {
+                            if(request()->has($field_name)){
+                                $value = request()->input($field_name);
+                                $submodel = $submodel->where($field, $value);
+                            }
                         }
                     }
-                  }
                 }
                 if(config('solunes.store')&&config('store.get_options_relation')&&$this->relation_cond){
                     $submodel = \CustomStore::get_options_relation($submodel, $this, $subnode, request()->segment(5));
