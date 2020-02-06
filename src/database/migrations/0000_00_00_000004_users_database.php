0d55e032daa5c71599eda0e33547a9d2b07d3fc3
Eduardo Mejia <edumejia30@gmail.com>
2020-02-06 14:57:22 -0400

    fix

--- src/database/migrations/0000_00_00_000004_users_database.php
+++ src/database/migrations/0000_00_00_000004_users_database.php
@@ -23,13 +23,13 @@ class UsersDatabase extends Migration
             $table->string('password', 60)->nullable();
             $table->enum('status', ['normal','ask_password','pending_confirmation','banned'])->default('normal');
             $table->boolean('notifications_email')->default(0);
-            if(config('solunes.send_notification_sms'){
+            if(config('solunes.send_notification_sms')){
                 $table->boolean('notifications_app')->default(0);
             }
-            if(config('solunes.send_notification_sms'){
+            if(config('solunes.send_notification_sms')){
                 $table->boolean('notifications_sms')->default(0);
             }
-            if(config('solunes.send_notification_whatsapp'){
+            if(config('solunes.send_notification_whatsapp')){
                 $table->boolean('notifications_whatsapp')->default(0);
             }
             $table->integer('notifications_last_read')->nullable();
