7d6ff8d6332122d0726abdabafd3e7e11fc94156
Eduardo Mejia <edumejia30@gmail.com>
2020-03-03 19:08:40 -0400

    fix

--- src/views/emails/verification-email.blade.php
+++ src/views/emails/verification-email.blade.php
@@ -1,8 +1,6 @@
 @extends('master::layouts/email')
 
-@section('icon')
-Profile
-@endsection
+@section('icon', 'Profile')
 
 @section('content')
 	<h2 style="font-family: Arial, Helvetica, sans-serif;margin-top: 16px;margin-bottom: 8px;word-break: break-word;font-size: 28px;line-height: 38px;font-weight: bold;">
