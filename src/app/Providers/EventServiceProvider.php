e653772cbd13e3f60f80fc8df3dba8a9f49ba073
Eduardo Mejia <edumejia30@gmail.com>
2020-05-02 14:29:52 -0400

    fix

--- src/app/Providers/EventServiceProvider.php
+++ src/app/Providers/EventServiceProvider.php
@@ -41,6 +41,8 @@ class EventServiceProvider extends ServiceProvider
             $events->listen('eloquent.creating: Solunes\Customer\App\CustomerNote', '\Solunes\Customer\App\Listeners\CustomerNoteCreating');
             $events->listen('eloquent.created: Solunes\Customer\App\CustomerNote', '\Solunes\Customer\App\Listeners\CustomerNoteCreated');
             $events->listen('eloquent.creating: Solunes\Customer\App\CustomerPayment', '\Solunes\Customer\App\Listeners\CustomerPaymentCreating');
+            $events->listen('eloquent.updating: Solunes\Customer\App\CustomerPayment', '\Solunes\Customer\App\Listeners\CustomerPaymentUpdating');
+            $events->listen('eloquent.deleting: Solunes\Customer\App\CustomerPayment', '\Solunes\Customer\App\Listeners\CustomerPaymentDeleting');
             // Suscripciones
             $events->listen('eloquent.creating: Solunes\Customer\App\CustomerSubscription', '\Solunes\Customer\App\Listeners\CustomerSubscriptionCreating');
             $events->listen('eloquent.created: Solunes\Customer\App\CustomerSubscription', '\Solunes\Customer\App\Listeners\CustomerSubscriptionCreated');
